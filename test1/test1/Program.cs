651cdac6bf7adf0ff5790644c76565c0de609965
m305-33\w401 <a106082030@mail.shu.edu.tw>
2018-03-15 18:06:45 +0800

    第一次

---
+++ test1/test1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.Write("請輸入身高(公尺):");
+            string height = Console.ReadLine();
+
+            Console.Write("請輸入體重(公斤)");
+            string weight = Console.ReadLine();
+
+            double total = double.Parse(weight) / (double.Parse(height) * double.Parse(height));
+            Console.WriteLine("您的BMI值為:" + total);
+
+            if(18.5 <= total && total < 24)
+            {
+                Console.WriteLine("您的體重正常");
+            }
+            else if (total >= 24)
+            {
+                Console.WriteLine("您的體重過重了");
+            }
+            else
+            {
+                Console.WriteLine("您的體重過輕了");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
